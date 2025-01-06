e9b65f2d5575bc9e171db1dedb752c7859efa33a
Rustam Kalimulin <r.kalimulin@jetmail.cc>
2025-01-06 15:02:45 +0400

    06 - Creating auth screens

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import {SignInCard} from "@/features/auth/sign-in-card";
+
+const SignInPage = () => {
+  return <SignInCard/>
+}
+
+export default SignInPage;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import {SignUpCard} from "@/features/auth/sign-up-card";
+
+const SignUpPage = () => {
+  return <SignUpCard />
+}
+
+export default SignUpPage;
