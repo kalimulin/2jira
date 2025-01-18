682e19ddf5b6e7c2e96d0f95f7d417567c139198
Rustam Kalimulin <r.kalimulin@jetmail.cc>
2025-01-18 21:32:18 +0400

    24 - Building the members list

---
+++ features/members/api/use-update-member.ts
@@ -0,0 +1,35 @@
+import {toast} from "sonner";
+import {InferRequestType, InferResponseType} from "hono";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+
+import {client} from "@/lib/rpc";
+
+type ResponseType = InferResponseType<typeof client.api.members[":memberId"]["$patch"], 200>;
+type RequestType = InferRequestType<typeof client.api.members[":memberId"]["$patch"]>;
+
+export const useUpdateMember = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    ResponseType,
+    Error,
+    RequestType
+  >({
+    mutationFn: async ({param, json}) => {
+      const response = await client.api.members[":memberId"]["$patch"]({param, json});
+
+      if (!response.ok) {
+        throw new Error("Failed to update member");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Member updated");
+      queryClient.invalidateQueries({queryKey: ["members"]});
+    },
+    onError: () => {
+      toast.error("Failed to update member");
+    }
+  });
+};
