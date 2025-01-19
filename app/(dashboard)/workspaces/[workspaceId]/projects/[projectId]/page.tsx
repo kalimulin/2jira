a8a1a86196351ec43ec641a94ea2fb118b54e77c
Rustam Kalimulin <r.kalimulin@jetmail.cc>
2025-01-19 16:30:10 +0400

    27 - Building a tasks API

--- app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -2,13 +2,13 @@ import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
 
-import { ProjectIdSettingsClient } from "./client";
+import { ProjectIdClient } from "./client";
 
-const ProjectIdSettingsPage = async () => {
+const ProjectIdPage = async () => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  return <ProjectIdSettingsClient />
+  return <ProjectIdClient />
 };
- 
-export default ProjectIdSettingsPage;
+
+export default ProjectIdPage;
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,12 +6,16 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateInviteCode(length: number) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length))
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-
   return result;
+}
+
+export function snakeCaseToTitleCase(str: string) {
+  return str.toLowerCase()
+    .replace(/_/g, " ")
+    .replace(/\b\w/g, (char) => char.toUpperCase())
 }
